3fe888d88cf95c3d4b211694bc4f83e419249fb3
BerAbreu12 <163349509+BerAbreu12@users.noreply.github.com>
2024-04-03 17:56:49 -0300

    Add files via upload

---
+++ moda.py
@@ -0,0 +1,14 @@
+
+print('\n Digite os valores abaixo \n')
+
+k = float(input("\n Digite o valor para K: "))
+theta = float(input("\n Digite o valor para theta: "))
+phi = float(input("\n Digite o valor para phi: "))
+
+print ("\n -----------------------")
+print("\n k = ", k)
+print("\n theta = ", theta)
+print("\n phi = ", phi)
+
+soma = k + theta + phi
+print("\n A soma dos valores eh: ", soma)
